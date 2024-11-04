c7053de47aa41d6e20a99a853c61b72c22c49e68
Dylan Càrdenas <cardenasdylan58@gmail.com>
2024-11-04 13:40:38 -0500

    Add files via upload

---
+++ pokemon/Principal.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pokemon;
+
+import static com.mycompany.pokemon.Estados.Ninguno;
+import static com.mycompany.pokemon.TipoPokemon.Fuego;
+
+
+
+/**
+ *
+ * @author Dylan
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+       Fuego Charmander= new Fuego("Charmander",39,52,Fuego,Ninguno);
+    }
+}
